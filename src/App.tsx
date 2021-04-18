2b3c723e312974b80cd9bf6f9ad289451c7f9b38
fuyoudai <2750432875@qq.com>
2021-04-18 10:05:55 +0800

    菜单按钮

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 // import './App.css'
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
       <header className="App-header">
         <Menu>
           <MenuItem>link1</MenuItem>
-          <MenuItem>link2</MenuItem>
+          <SubMenu title="title">
+            <MenuItem>link2-1</MenuItem>
+            <MenuItem>link2-2</MenuItem>
+          </SubMenu>
           <MenuItem>link3</MenuItem>
         </Menu>
       </header>
