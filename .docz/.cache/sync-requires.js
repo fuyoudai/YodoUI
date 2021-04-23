b81067734efe8d7cf3fcb04d37219d8f92cfb3fb
fuyoudai <2750432875@qq.com>
2021-04-23 23:15:51 +0800

    docz写组件库文档

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("D:\\projects\\YodoUI\\.docz\\.cache\\dev-404-page.js"))),
+  "component---readme-md": (preferDefault(require("D:\\projects\\YodoUI\\README.md"))),
+  "component---src-components-home-mdx": (preferDefault(require("D:\\projects\\YodoUI\\src\\components\\home.mdx"))),
+  "component---src-pages-404-js": (preferDefault(require("D:\\projects\\YodoUI\\.docz\\src\\pages\\404.js")))
+}
+
