d1351ad816eb27681c3ce6da870f286c43a13146
fuyoudai <2750432875@qq.com>
2021-04-24 23:53:59 +0800

    格式化代码

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -5,15 +5,15 @@ export type ButtonSize = 'large' | 'small'
 export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
-  className?: string;
+  className?: string
   /**设置 Button 的禁用 */
-  disabled?: boolean;
+  disabled?: boolean
   /**设置 Button 的尺寸 */
-  size?: ButtonSize;
+  size?: ButtonSize
   /**设置 Button 的类型 */
-  buttonType?: ButtonType;
-  children: React.ReactNode;
-  href?: string;
+  buttonType?: ButtonType
+  children: React.ReactNode
+  href?: string
 }
 /**
  ButtonHTMLAttributes<HTMLElement> react提供了button标签的原生属性
@@ -26,13 +26,13 @@ export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 /**
  * 页面中最常用的的按钮元素，适合于完成特定的交互
  * ### 引用方法
- * 
+ *
  * ~~~js
  * import { Button } from 'vikingship'
  * ~~~
  */
 export const Button: FC<ButtonProps> = (props) => {
-  const { 
+  const {
     buttonType,
     className,
     disabled,
@@ -45,25 +45,17 @@ export const Button: FC<ButtonProps> = (props) => {
   const classes = classNames('btn', className, {
     [`btn-${buttonType}`]: buttonType,
     [`btn-${size}`]: size,
-    'disabled': (buttonType === 'link') && disabled
+    disabled: buttonType === 'link' && disabled,
   })
-  if (buttonType === 'link' && href ) {
+  if (buttonType === 'link' && href) {
     return (
-      <a
-        className={classes}
-        href={href}
-        {...restProps}
-      >
+      <a className={classes} href={href} {...restProps}>
         {children}
       </a>
     )
   } else {
     return (
-      <button
-        className={classes}
-        disabled={disabled}
-        {...restProps}
-      >
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     )
@@ -72,7 +64,7 @@ export const Button: FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  buttonType: 'default'
+  buttonType: 'default',
 }
 
-export default Button;
+export default Button
