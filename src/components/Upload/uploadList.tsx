f9404d11ca5a86d30788e43bf9a5bb4304ca23fc
fuyoudai <2750432875@qq.com>
2021-05-06 22:55:49 +0800

    uploadList优化

--- src/components/Upload/uploadList.tsx
+++ src/components/Upload/uploadList.tsx
@@ -3,19 +3,16 @@ import { UploadFile } from './upload'
 import Icon from '../Icon/icon'
 import Progress from '../Progress/progress'
 interface UploadListProps {
-  fileList: UploadFile[];
-  onRemove: (_file: UploadFile) => void;
+  fileList: UploadFile[]
+  onRemove: (_file: UploadFile) => void
 }
 
 export const UploadList: FC<UploadListProps> = (props) => {
-  const {
-    fileList,
-    onRemove,
-  } = props
+  const { fileList, onRemove } = props
 
   return (
     <ul className="viking-upload-list">
-      {fileList.map(item => {
+      {fileList.map((item) => {
         return (
           <li className="viking-upload-list-item" key={item.uid}>
             <span className={`file-name file-name-${item.status}`}>
@@ -23,18 +20,27 @@ export const UploadList: FC<UploadListProps> = (props) => {
               {item.name}
             </span>
             <span className="file-status">
-              {(item.status === 'uploading' || item.status === 'ready') && <Icon icon="spinner" spin theme="primary" />}
-              {item.status === 'success' && <Icon icon="check-circle" theme="success" />}
-              {item.status === 'error' && <Icon icon="times-circle" theme="danger" />}
+              {(item.status === 'uploading' || item.status === 'ready') && (
+                <Icon icon="spinner" spin theme="primary" />
+              )}
+              {item.status === 'success' && (
+                <Icon icon="check-circle" theme="success" />
+              )}
+              {item.status === 'error' && (
+                <Icon icon="times-circle" theme="danger" />
+              )}
             </span>
             <span className="file-actions">
-              <Icon icon="times" onClick={() => { onRemove(item)}}/>
-            </span>
-            {item.status === 'uploading' && 
-              <Progress 
-                percent={item.percent || 0}
+              <Icon
+                icon="times"
+                onClick={() => {
+                  onRemove(item)
+                }}
               />
-            }
+            </span>
+            {item.status === 'uploading' && (
+              <Progress percent={item.percent || 0} />
+            )}
           </li>
         )
       })}
@@ -42,4 +48,4 @@ export const UploadList: FC<UploadListProps> = (props) => {
   )
 }
 
-export default UploadList;
+export default UploadList
